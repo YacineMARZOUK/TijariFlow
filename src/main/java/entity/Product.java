1ca0093cce4a10dec39defdd251a06248b65e133
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-24 15:01:48 +0100

    edit the Product entity

--- src/main/java/entity/Product.java
+++ src/main/java/entity/Product.java
@@ -1,4 +1,29 @@
 package entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nom;
+
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal prixUnitaireHT;
+
+    private Integer stockDisponible;
+
+
+    private boolean estSupprime = false;
 }
