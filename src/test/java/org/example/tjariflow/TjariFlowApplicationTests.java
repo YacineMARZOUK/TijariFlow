29f5f31af88ced7332635f6bcb04b68de08ae672
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-24 12:20:48 +0100

    set up the architecture

---
+++ src/test/java/org/example/tjariflow/TjariFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tjariflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjariFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
