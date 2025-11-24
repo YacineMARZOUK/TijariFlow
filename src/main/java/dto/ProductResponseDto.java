0127d3a8a30d2e626ffc0b1fa10131b127237b12
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-24 15:27:24 +0100

    add the ProductResponseDto

---
+++ src/main/java/dto/ProductResponseDto.java
@@ -0,0 +1,14 @@
+package dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductResponseDto {
+    private Long id;
+    private String nom;
+    private BigDecimal prixUnitaireHT;
+    private Integer stockDisponible;
+
+}
