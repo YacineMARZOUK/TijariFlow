d8f2e508a9e77d3501e592785bc6d537837f9f13
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-24 14:06:18 +0100

    add the User entity

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,31 @@
+package entity;
+
+import enums.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username; // Utilisé pour la connexion
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @OneToOne(mappedBy = "user")
+    private Client client;
+}
