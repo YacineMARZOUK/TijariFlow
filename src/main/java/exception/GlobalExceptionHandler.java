61ecc702c275ca26b61bbd94c1ab0405c22337e0
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-25 09:49:51 +0100

    create handleBusinessRuleViolation to handle the Business Rule Violation in the GlobalExceptionHandler

--- src/main/java/exception/GlobalExceptionHandler.java
+++ src/main/java/exception/GlobalExceptionHandler.java
@@ -4,18 +4,34 @@ import dto.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import java.time.LocalDateTime;
 
 public class GlobalExceptionHandler {
 
     //404
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponse>handleResourceNotFound(ResourceNotFoundException ex, HttpServletRequest request){
         HttpStatus status = HttpStatus.NOT_FOUND;
         ErrorResponse error = ErrorResponse.builder()
                 .timestamp(LocalDateTime.now())
                 .status(status.value())
-                .errorType("Not Found")
+                .errorType("Resource Not Found")
+                .message(ex.getMessage())
+                .path(request.getRequestURI())
+                .build();
+        return new ResponseEntity<>(error, status);
+    }
+
+    //422
+    @ExceptionHandler(BusinessRuleViolationException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessRuleViolation(BusinessRuleViolationException ex, HttpServletRequest request){
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ErrorResponse error = ErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .errorType("Business Rule Violation")
                 .message(ex.getMessage())
                 .path(request.getRequestURI())
                 .build();
