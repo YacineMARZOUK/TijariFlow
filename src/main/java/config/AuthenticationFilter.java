ecc765b63c5bcb47037d6928daa5a9b37e9212ab
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-26 10:05:33 +0100

    create AuthenticationFilter class with session verification & public endpoints verification

---
+++ src/main/java/config/AuthenticationFilter.java
@@ -0,0 +1,39 @@
+package config;
+
+import controller.AuthController;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+import java.util.logging.Filter;
+
+public class AuthenticationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletRequest;
+        String path = request.getRequestURI();
+        HttpSession session = request.getSession(false);
+
+        // the endpoints publiccc
+        if(path.startsWith("/api/auth/") || path.equals("/") || path.endsWith("/swagger-ui.html") || path.contains("webjars") || path.contains("v3/api-docs")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        // session verificationn
+        if (session == null || session.getAttribute(AuthController.SESSION_USER_KEY) == null){
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.getWriter().write("{\\\"status\\\": 401, \\\"errorType\\\": \\\"Unauthorized\\\", \\\"message\\\": \\\"Authentification requise.\\\"}\"");
+            return;
+        }
+    }
+
+}
