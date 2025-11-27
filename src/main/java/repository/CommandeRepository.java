0a137dd633a2a628ce47036d9489413889f7abc4
YacineMARZOUK <yacinemarzou@gmail.com>
2025-11-27 09:40:48 +0100

    create the CommandeRepository

---
+++ src/main/java/repository/CommandeRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.Commande;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommandeRepository extends JpaRepository<Commande, Long > {
+}
