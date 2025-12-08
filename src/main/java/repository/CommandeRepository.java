542e8c49a77fd020e2cf90078de96a7e4a762710
YacineMARZOUK <yacinemarzou@gmail.com>
2025-12-08 14:33:27 +0100

    merge the CommandeRepository

--- src/main/java/repository/CommandeRepository.java
+++ src/main/java/repository/CommandeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
 public interface CommandeRepository extends JpaRepository<Commande, Long> {
     List<Commande> findByClientId(Long clientId);
 }
